6e592e3d225cd0987dabc2329b81db852233b3aa
Halimi <halimi@Halimis-MacBook-Pro.local>
2020-03-22 23:25:59 +0800

    add first GET route

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 
-
-
+const mockUserData = [
+    {name: 'Mark'},
+    {name: 'Jill'}
+]
+app.get('/users', function(req, res){
+    res.json({
+        success: true,
+        message: 'Successfully got users. Nice!',
+        users: mockUserData
+    })
+})
 
 
 
