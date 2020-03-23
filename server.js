67490a8af2ef04ae678aea59d87f8dbaacf8bbc6
Halimi <halimi@Halimis-MacBook-Pro.local>
2020-03-23 17:16:59 +0800

    add login POST route

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
 
 const mockUserData = [
     {name: 'Mark'},
     {name: 'Jill'}
 ]
-app.get('/users/:id', function(req, res){
+
+app.get('/users', function(req, res) {
+    res.json({
+        success: true,
+        message: 'successfully got users. nice!',
+        users: mockUserData
+    })
+})
+
+app.get('/users/:id', function(req, res) {
     console.log(req.params.id)
     res.json({
         success: true,
@@ -14,6 +26,27 @@ app.get('/users/:id', function(req, res){
     })
 })
 
+app.post('/login', function(req, res) {
+    // typically passwords are encypted using something like bcyrpt before sending to database
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
+})
 
 
 app.listen(8000, () => console.log("server is running"));
